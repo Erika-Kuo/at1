8ee4819ffb54fe410e072895469ffa578babc32d
Erika Kuo <Erika Kuo>
2024-03-30 10:52:54 +1100

    Oops

--- at1/settings.py
+++ at1/settings.py
@@ -1,5 +1,28 @@
 import os
+import logging
 
+logger = logging.getLogger(__name__)
+
+class IndexView(ListView):
+    template_name = "hello/index.html"
+    model = VocabularyWord
+
+    def get(self, request, *args, **kwargs):
+        logger.info("Starting to render the index page...")
+        response = super().get(request, *args, **kwargs)
+        logger.info("Finished rendering the index page.")
+        return response
+
+class DetailView(DetailView):
+    template_name = "hello/detail.html"
+    model = VocabularyWord
+
+    def get(self, request, *args, **kwargs):
+        logger.info("Starting to render the detail page...")
+        response = super().get(request, *args, **kwargs)
+        logger.info("Finished rendering the detail page.")
+        return response
+    
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -58,6 +81,24 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'myapp': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 
 WSGI_APPLICATION = 'at1.wsgi.application'
 
--- eduprod/views.py
+++ eduprod/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import VocabularyWord
+import logging
+
+logger = logging.getLogger(__name__)
 
 class IndexView(ListView):
     template_name = "hello/index.html"
     model = VocabularyWord
 
+    def get(self, request, *args, **kwargs):
+        logger.info("Starting to render the index page...")
+        response = super().get(request, *args, **kwargs)
+        logger.info("Finished rendering the index page.")
+        return response
+
 class DetailView(DetailView):
     template_name = "hello/detail.html"
     model = VocabularyWord
+
+    def get(self, request, *args, **kwargs):
+        logger.info("Starting to render the detail page...")
+        response = super().get(request, *args, **kwargs)
+        logger.info("Finished rendering the detail page.")
+        return response
+    
+    
